a5449aea44930b0df88b84a36694bf2b0ffbf5c2
brelee2222 <brelee1842@outlook.com>
2024-09-13 22:53:38 -0700

    Changes

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
         AudioInputStream audio = AudioSystem.getAudioInputStream(new File(args[0]));
+        float sampleRate = audio.getFormat().getSampleRate();
+        int pulseRate = 2;
         byte[] buffer = audio.readAllBytes();
 
         double scale = .25F;
-        short addRange = 30600;
+        short addRange = 15400;
         short add = addRange;
         int addInterpolate = add;
 
@@ -19,22 +21,23 @@ public class Main {
         for(int bufferIndex = 0; bufferIndex < buffer.length; bufferIndex += 2) {
             short frame = (short) (((buffer[bufferIndex+1] & 0xff) << 8) | (buffer[bufferIndex] & 0xff));
 
-            addInterpolate = (addInterpolate * 2000 + add) / 2001;
+//            addInterpolate = (addInterpolate * 10000 + add) / 10001;
 
             prevAvg = (short) ((prevAvg * 199 + Math.abs(frame)) / 200);
 
-            flipcooldown--;
-
-            if(Math.abs(prevAvg) < 10 && Math.abs(frame) < 127) {
-                if(flipcooldown < 0) {
-                    add = (short) ((Math.random() - 0.5) * 2 * addRange);
-//                    frame = add;
-                    flipcooldown = flipStartCooldown;
-                }
-            }
+//            flipcooldown--;
+//
+//            if(Math.abs(prevAvg) < 10 && Math.abs(frame) < 127) {
+//                if(flipcooldown < 0) {
+//                    add = (short) ((Math.random() - 0.5) * 2 * addRange);
+////                    frame = add;
+//                    flipcooldown = flipStartCooldown;
+//                }
+//            }
 
             frame *= scale;
-            frame += addInterpolate;
+//            frame += addInterpolate;
+            frame += Math.sin(bufferIndex/sampleRate/pulseRate) * addRange;
 //            System.out.println(buffer[bufferIndex]);
             buffer[bufferIndex+1] = (byte) (frame >> 8);
 //            buffer[bufferIndex] *= scale;
