91405230c26c27e162bbf6bb45d1e41075d9807c
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2024-09-13 23:50:22 -0700

    Added frequency

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,22 @@ public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
         AudioInputStream audio = AudioSystem.getAudioInputStream(new File(args[0]));
         float sampleRate = audio.getFormat().getSampleRate();
-        int pulseRate = 2;
+        double pulseRate = 0.07;
+        double secondaryRate = 0.04;
+
+//        double x = 0;
+
+//        double xMaxRate = 60;
+
+//        double xRate = xMaxRate;
+
+//        double highPulseRate = 0.00003571428;
         byte[] buffer = audio.readAllBytes();
 
         double scale = .25F;
         short addRange = 15400;
-        short add = addRange;
-        int addInterpolate = add;
+//        short add = addRange;
+//        int addInterpolate = add;
 
         int flipStartCooldown = 60000;
         int flipcooldown = 0;
@@ -20,6 +29,10 @@ public class Main {
 
         for(int bufferIndex = 0; bufferIndex < buffer.length; bufferIndex += 2) {
             short frame = (short) (((buffer[bufferIndex+1] & 0xff) << 8) | (buffer[bufferIndex] & 0xff));
+
+//            x += xRate;
+
+//            xRate = (xRate * 2000 + Math.random() * xMaxRate) / 20001;
 
 //            addInterpolate = (addInterpolate * 10000 + add) / 10001;
 
@@ -37,7 +50,7 @@ public class Main {
 
             frame *= scale;
 //            frame += addInterpolate;
-            frame += Math.sin(bufferIndex/sampleRate/pulseRate) * addRange;
+            frame += (Math.sin(bufferIndex*2/sampleRate/pulseRate) + Math.sin(bufferIndex*2/sampleRate/secondaryRate))/2 * addRange;
 //            System.out.println(buffer[bufferIndex]);
             buffer[bufferIndex+1] = (byte) (frame >> 8);
 //            buffer[bufferIndex] *= scale;
